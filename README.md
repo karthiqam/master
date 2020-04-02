61dffc521ca7a29d8d8f358e9deda873bcd85987
Karthik <karthiqam@gmail.com>
2020-04-02 21:45:18 +0530

    Master - Bug fix applied

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # master
+# 04/02/2020 09:30pm - Bug fix applied
--- hello-world.py
+++ hello-world.py
@@ -2,6 +2,8 @@ print("Hello World!")
 
 print("Hello guys & girls!")
 
-print("Master - First commit")
+print("Master - First commit completed")
 
-print("Master - Second commit")
+print("Master - Second commit completed")
+
+print("Master - Bug fix applied")
